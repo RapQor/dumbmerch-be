41c17cdd9592531936f1e18a6d23899f3091f27d
RafiAbqor <rafi.abqor@gmail.com>
2024-10-21 17:58:13 +0700

    change: cors api

--- index.ts
+++ index.ts
@@ -24,7 +24,7 @@ app.use("/uploads", express.static("src/uploads"));
 app.use(route);
 app.use(
   cors({
-    origin: "http://localhost:5173/",
+    origin: "https://dumbmerch-dun.vercel.app/",
     methods: "GET,POST,PATCH,DELETE,OPTIONS",
     allowHeaders: ["Content-Type", "Authorization", "Multipart/form-data"],
   })
